2a80062015b0162cef742742e6df984a582ab43d
Jure Cotic <ssitcm@main.futurama.si>
2014-09-02 14:16:42 -0400

    unique visitors and unique visitors by the hour added

--- locations/views.py
+++ locations/views.py
@@ -8,10 +8,13 @@ from ipware.ip import get_real_ip
 from datetime import datetime
 import GeoIP
 from django.conf import settings
+from django.db.models import Count
 
 def index(request):
 	user_list = Location.objects.all()
 	ip = get_real_ip(request)
+	p = Location.objects.raw('SELECT id, count(distinct(user_ip)) as c from locations_location')[0]
+	hours= Location.objects.raw("select id,strftime('%Y-%m-%dT%H:00:00.000', visit_date) as h,time(strftime('%Y-%m-%dT%H:00:00.000', visit_date),'localtime') as time,count(distinct(user_ip)) as c from locations_location where strftime('%Y-%m-%d', visit_date) =  strftime('%Y-%m-%d', DATE('now','-1 days')) group by strftime('%Y-%m-%dT%H:00:00.000', visit_date) ")
 	
 	gi = GeoIP.open(settings.GEO_IP_DB, GeoIP.GEOIP_STANDARD)
 	gir = gi.record_by_addr(ip)
@@ -26,5 +29,7 @@ def index(request):
 		'ip': ip,
 		'lat':gir['latitude'],
 		'lon':gir['longitude'],
+		'p':p.c,
+		'hours':hours,
 	})
 	return HttpResponse(template.render(context))
